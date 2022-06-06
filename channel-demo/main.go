79b320ab6c1fca5748217d9ef0d255e6af12c344
YuJianzhou <yujianzhou@aidlux.com>
2022-06-06 09:45:59 +0800

    è®多个goroutine竞争一个channel数据

--- channel-demo/main.go
+++ channel-demo/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	ch := getEmptyChannel()
+	/*ch := getEmptyChannel()
 	fmt.Println("start to select")
 
 	select {
@@ -14,7 +14,49 @@ func main() {
 		fmt.Println("read from timer")
 	case <-ch:
 		fmt.Println("read from nil channel")
+	}*/
+
+
+	ch := make(chan int)
+
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			for {
+				req := <- ch
+				fmt.Printf("worker[%d] got v from channel is [%d]\n", i, req)
+			}
+		}(i)
 	}
+
+	fmt.Println("start to write v into channel.")
+	time.Sleep(2 * time.Second)
+
+	//go func() {
+	//	v := 0
+	//	for {
+	//
+	//		ch <- v
+	//		v++
+	//		//time.Sleep(2 * time.Second)
+	//	}
+	//}()
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	//time.Sleep(1 * time.Second)
+	ch <- 5
+	ch <- 6
+	ch <- 7
+	ch <- 8
+	//time.Sleep(1 * time.Second)
+	ch <- 9
+	ch <- 10
+	ch <- 11
+	ch <- 12
+	//time.Sleep(1 * time.Second)
+
+	time.Sleep(1 * time.Second)
 }
 
 func getEmptyChannel() <-chan struct{}{
